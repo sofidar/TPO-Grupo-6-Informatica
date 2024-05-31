253b2f45559e2ca543f494ca9ea89a72c5e467e5
valsalama <vsalama@uade.edu.ar>
2024-05-31 10:59:18 -0300

    Update TPO.py

--- TPO.py
+++ TPO.py
@@ -1,1 +1,6 @@
-#asdf
+#una fucnión que ingrese datos (las dos listas)
+#una función que sea el método de inserción
+#una función que de total vendido por vendedor
+#función de promedio de ventas x vendedor
+# función del vendedor con mayor cantidad de ventas --> buscar 
+#función de mayor venta realizada --> buscar el mayor valor de la lista de ventas
