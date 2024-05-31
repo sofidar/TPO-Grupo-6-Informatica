cc495fc902296d385240d39453e34f4d14c73bbc
valsalama <vsalama@uade.edu.ar>
2024-05-31 10:59:47 -0300

    Update TPO.py

--- TPO.py
+++ TPO.py
@@ -1,3 +1,11 @@
+#Una empresa cuenta con un plantel de vendedores, numerados con valores positivos arbitrarios. Por cada venta realizada se ingresa por teclado el número de vendedor y el importe
+#de la misma, donde un número de vendedor -1 indica el final de los datos. Realizar un programa para imprimir:
+#· Total vendido por cada vendedor, ordenado de mayor a menor según el total vendido.
+#· Importe de la venta promedio por vendedor.
+#· Número de vendedor con mayor cantidad de ventas.
+#· Importe y número de vendedor correspondiente a la mayor venta realizada.
+#La cantidad de vendedores no se conoce. Este dato deberá deducirse se las ventas ingresadas.
+
 #una fucnión que ingrese datos (las dos listas)
 #una función que sea el método de inserción
 #una función que de total vendido por vendedor
